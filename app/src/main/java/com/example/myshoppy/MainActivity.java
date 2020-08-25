10ab882582356ee013804cd100f07ed9a332d858
Aswin Ramesh <aswinramesh527@gmail.com>
2020-08-25 20:56:13 +0530

    Version 1

---
+++ app/src/main/java/com/example/myshoppy/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myshoppy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    public static int SPLASH_TIME_OUT=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent=new Intent(MainActivity.this,Home.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
